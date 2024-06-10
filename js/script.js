4cfdfade993f653e27c30a313be892ce333b9b8e
Shohelrana63 <shohelrana.beg@gmail.com>
2024-06-10 19:26:45 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.slider_part').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    arrows: true,
+    prevArrow: '<i class="fa-solid fa-circle-arrow-left prev"></i>',
+    nextArrow:'<i class="fa-solid fa-circle-arrow-right next"></i>',
+    dots:true,
+  });
